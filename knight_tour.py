3d0c77f0ee675e07168ae15d95284be6f63a71ad
man2nainwal <man2nainwal@users.noreply.github.com>
2017-05-03 11:21:30 +0530

    Update knight_tour.py
    add functions: valid_move and print_box

--- knight_tour.py
+++ knight_tour.py
@@ -3,4 +3,12 @@ from __future__ import print_function
 square = 8
 
 def valid_move (x, y, box):
+  return (x >= 0 and x < square and y >= 0 and y < square and box[x][y] == -1)
+
+def print_box(box):
+  for i in range(square):
+    for j in range(square):
+      print(box[i][j],)
+    print()
+    
 
